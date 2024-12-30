ef5b216faabc5613a01e57b7bbc7c99897fc785a
imevanc <charal.vag@gmail.com>
2024-12-30 23:09:08 +0000

    feat: update the payhive template

--- test/pages/Home.spec.tsx
+++ test/pages/Home.spec.tsx
@@ -7,23 +7,12 @@ test("render the Homepage", async () => {
   renderPage(<Home />);
   expect(
     screen.getByRole("heading", {
-      name: /Unleashing Creativity: Powered by Generative AI/i,
+      name: /PayHive/i,
     }),
   ).toBeVisible();
   expect(
-    screen.getByText("Announcing our next round of funding."),
+    screen.getByRole("heading", {
+      name: /Split bills with friends, track memberships, and manage shared expenses./i,
+    }),
   ).toBeVisible();
-  expect(
-    screen.getByText(
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eleifend tempor nisi, eget suscipit ante elementum ac. Praesent euismod arcu lacus, sit amet mollis libero egestas vel.",
-    ),
-  ).toBeVisible();
-  expect(screen.getByRole("link", { name: /read more/i })).toBeVisible();
-  expect(screen.getByRole("link", { name: /get started/i })).toBeVisible();
-  expect(screen.getByRole("link", { name: /learn more/i })).toBeVisible();
-  expect(screen.getByRole("link", { name: /log in/i })).toBeVisible();
-  expect(screen.getByTestId("Product-desktop")).toBeVisible();
-  expect(screen.getByTestId("Features-desktop")).toBeVisible();
-  expect(screen.getByTestId("Marketplace-desktop")).toBeVisible();
-  expect(screen.getByTestId("Company-desktop")).toBeVisible();
 });
