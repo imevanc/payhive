3f56c4c5bca00af3398c6f326ed5aa188a4090e9
unknown <mikeatkoo01@gmail.com>
2025-08-15 20:46:38 +0100

    feat(sign-up): useform implentation solving sonar

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -132,7 +132,8 @@ export default function SignUpPage() {
               {...register("email", {
                 required: "Email is required",
                 pattern: {
-                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+
                   message: "Please enter a valid email address",
                 },
               })}
